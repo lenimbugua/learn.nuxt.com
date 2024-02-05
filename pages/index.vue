e700a1d69bc62353cdd666892ecb344b7f59836f
Leonard Mbugua <lenimbugua@gmail.co>
2024-02-05 15:58:24 +0300

    feat: color mode

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script setup>
+const { x, y } = useMouse();
+
+const colorMode = useColorMode();
+
+console.log(colorMode.value);
+</script>
+<template>
+  <div>
+    <h1>Hello World {{ x }} {{ y }}</h1>
+  </div>
+  <button
+    @click="colorMode.value = colorMode.value === 'light' ? 'dark' : 'light'"
+  >
+    Toggle Color mode
+  </button>
+</template>
+
+<style>
+.dark-mode body {
+  background-color: #091a28;
+  color: #ebf4f1;
+}
+</style>
